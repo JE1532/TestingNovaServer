0d7ab33a61f766f350aa4c9406c9c42a25fea44d
JE1532 <116882785+JE1532@users.noreply.github.com>
2024-02-08 15:29:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 int main() {
     printf("Hello, World!\n");
+    for ( sum = 0, i = 1; i <=n; ++i )
+        sum += i;
     printf("Counting to 10!");
     printf("Done counting!");
     return 0;
